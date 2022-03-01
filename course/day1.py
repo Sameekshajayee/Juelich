5226989abd88ed2f783594fb70fca9112300be11
sameeksha <s6sajaye@uni-bonn.de>
2022-03-01 15:41:02 +0100

    Temp

--- course/day1.py
+++ course/day1.py
@@ -1,3 +1,27 @@
-print("test")
+from os.path import join
+import SimpleITK as sitk
+import numpy as np
+import matplotlib.pyplot as plt
 
-print("new line")
+base = r"C:\Users\samee\Masters Internship\mr_pet\mr_pet"
+
+file = "FE1BP469M-BI_0000.nii.gz"
+
+
+file_path = join(base, file)
+sitk_image = sitk.ReadImage(file_path)
+
+
+plt.imshow(sitk_array[140, 100:140, 140], cmap='gray')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
